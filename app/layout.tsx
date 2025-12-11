bf2e028526cf189e4343910ce32a0d2990a8f325
Deivid Sandoval Cid <deivid.sandoval.cid@gmail.com>
2025-12-10 23:05:09 -0300

    add authprovider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
+import { AuthProvider } from "@/components/auth-provider"
 
 const geistSans = Geist({
   subsets: ["latin"],
@@ -26,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body className="antialiased">
-        {children}
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   ) 
